010a6b8e37a8abd55084f6e18f6fbd834b571704
Matthew Whitaker <matthew.w3000@gmail.com>
2016-12-29 20:23:47 -0700

    Update resultscript.js

--- resultscript.js
+++ resultscript.js
@@ -1,7 +1,7 @@
 //Copyright (c) Matthew Whitaker 2016
 function getResults(searchQuery) {
   var resultDiv = document.getElementById("inner_results");
-  switch(searchQuery.replace(/['!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~']/g,"")) {
+  switch(searchQuery.replace(/['!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~']/g,"").toLowerCase()) {
       //SHOW ME THE SUN-------------------------------------------------------------------------------------------------
     case "show me the sun":
       resultDiv.innerHTML = getImageResultString("Here is an image of the sun right now, constructed by compiling the two STEREO spacecraft's images of the sun with the image of the sun from the Earth. <br />The animation starts at 0&deg; (What we can see from Earth right now), and rotates to show the sun from different angles.", "https://stereo.gsfc.nasa.gov/beacon/euvi_195_rotated.gif");
