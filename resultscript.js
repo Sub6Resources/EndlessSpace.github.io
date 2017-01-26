bbc11025e81dcd362497708e5aea3864155f8de2
Matthew Whitaker <matthew.w3000@gmail.com>
2017-01-26 11:10:58 -0700

    Update resultscript.js

--- resultscript.js
+++ resultscript.js
@@ -16,8 +16,13 @@ function getResults(searchQuery) {
         resultDiv.innerHTML += getImageResultString("Here is a visible light image of the sun as seen from the STEREO Behind satellite. <br />It was last updated at the date and time noted on the bottom of the image.", "https://stereo-ssc.nascom.nasa.gov/beacon/latest_256/behind_euvi_304_latest.jpg");
       }
       break;
+      //STEREO-------------------------------------------------------------------------------------------------
     case "stereo":
       resultDiv.innerHTML = getFullSizeImageResultString("Here are the current positions of the STEREO Ahead and Behind spacecraft<br />Note that the position is relative to Mercury, Venus, and the Earth", "https://stereo-ssc.nascom.nasa.gov/where/where_is_stereo.gif");
+      break;
+      //HELP-------------------------------------------------------------------------------------------------
+    case "help":
+      resultdiv.innerHTML = getTextBoldResultString("There are several options for finding data on this site. You can either search for a satellite or other data (ex. Search 'STEREO'). Or try searching for images of a certain planet or object. (ex. Search 'Show me the sun').", "Help");
       break;
     default:
       resultDiv.innerHTML = getTextResultString('No live data found on this site for "'+searchQuery+'"');
